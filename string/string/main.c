3b62d0e6e3943ae658195b22578a8072d061be2a
yuxiPhilipp <yuxi_philipp@hotmail.com>
2025-01-04 22:32:51 -0500

    update hello world

--- string/string/main.c
+++ string/string/main.c
@@ -11,8 +11,6 @@
 int main(int argc, const char * argv[]) {
     // insert code here........
     printf("Hello, World!\n");
-    printf("Hello, World!\n");
-    printf("Hello, World!\n");
 
     return 0;
 }
