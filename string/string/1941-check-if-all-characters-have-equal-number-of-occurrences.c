33bb0cb5c11eef47eb8e4b2bf89a8281b1a36bf6
yuxiPhilipp <yuxi_philipp@hotmail.com>
2025-01-04 22:31:33 -0500

    commit string proj

---
+++ string/string/1941-check-if-all-characters-have-equal-number-of-occurrences.c
@@ -0,0 +1,9 @@
+//
+//  1941-check-if-all-characters-have-equal-number-of-occurrences.c
+//  string
+//
+//  Created by xiy3 on 2025-01-04.
+//  Copyright © 2025 Kabilou. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ string/string/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  string
+//
+//  Created by xiy3 on 2025-01-04.
+//  Copyright © 2025 Kabilou. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here........
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
+
+    return 0;
+}
