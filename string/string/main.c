b6ba6466cc85cd2ca4bab3c136ffa7b0403fff62
yuxiPhilipp <yuxi_philipp@hotmail.com>
2025-01-04 22:33:31 -0500

    modify main.c

--- string/string/main.c
+++ string/string/main.c
@@ -11,6 +11,8 @@
 int main(int argc, const char * argv[]) {
     // insert code here........
     printf("Hello, World!\n");
+    printf("Hello, World!\n");
+    printf("Hello, World!\n");
 
     return 0;
 }
